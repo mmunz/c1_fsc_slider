e6dec027f1d82a707b833cf0d5243c90c7ffdbcc
Manuel Munz <munz@comuno.net>
2015-12-29 12:32:37 +0100

    more renaming

--- Resources/Public/JavaScript/C1FscSlider.js
+++ Resources/Public/JavaScript/C1FscSlider.js
@@ -2,7 +2,7 @@
 (function ($) {
     'use strict';
     $(document).ready(function () {
-        var $slider = $('.fluid-styled-slider');
+        var $slider = $('.c1-fsc-slider');
         if ($slider.length) {
             $slider.slick($slider.data('options'));
         }
