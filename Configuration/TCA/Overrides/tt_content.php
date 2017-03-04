5564f92f42ed86d7d2d8f92d8dcdd27881b17f91
Manuel Munz <munz@comuno.net>
2017-03-04 14:28:50 +0100

    remove dependency on c1-fluid-styled-responsive-images, better TYPO3 v8 support

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,6 +3,8 @@ defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
 
+    $typo3_major_version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version();
+
     $languageFilePrefix = 'LLL:EXT:fluid_styled_content/Resources/Private/Language/Database.xlf:';
     $customLanguageFilePrefix = 'LLL:EXT:c1_fsc_slider/Resources/Private/Language/locallang_be.xlf:';
     $frontendLanguageFilePrefix = 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:';
@@ -23,8 +25,11 @@ call_user_func(function () {
     $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['fsc_slider'] = $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['textmedia'];
 
     // Define what fields to display
-    $GLOBALS['TCA']['tt_content']['types']['fsc_slider'] = [
-        'showitem'         => '
+
+    if (intval($typo3_major_version[0]) < 8) {
+
+        $GLOBALS['TCA']['tt_content']['types']['fsc_slider'] = [
+            'showitem' => '
                 --palette--;' . $frontendLanguageFilePrefix . 'palette.general;general,
                 image_format,
                 --palette--;' . $languageFilePrefix . 'tt_content.palette.mediaAdjustments;mediaAdjustments,
@@ -34,19 +39,52 @@ call_user_func(function () {
             --div--;' . $customLanguageFilePrefix . 'tca.tab.sliderElements,
                  assets
         ',
-        'columnsOverrides' => [
-            'media' => [
-                'label'  => $languageFilePrefix . 'tt_content.media_references',
-                'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig('media', [
-                    'appearance'    => [
-                        'createNewRelationLinkTitle' => $languageFilePrefix . 'tt_content.media_references.addFileReference'
-                    ],
-                    // custom configuration for displaying fields in the overlay/reference table
-                    // behaves the same as the image field.
-                    'foreign_types' => $GLOBALS['TCA']['tt_content']['columns']['image']['config']['foreign_types']
-                ], $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext'])
+            'columnsOverrides' => [
+                'media' => [
+                    'label' => $languageFilePrefix . 'tt_content.media_references',
+                    'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig('media', [
+                        'appearance' => [
+                            'createNewRelationLinkTitle' => $languageFilePrefix . 'tt_content.media_references.addFileReference'
+                        ],
+                        // custom configuration for displaying fields in the overlay/reference table
+                        // behaves the same as the image field.
+                        'foreign_types' => $GLOBALS['TCA']['tt_content']['columns']['image']['config']['foreign_types']
+                    ], $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext'])
+                ]
             ]
-        ]
-    ];
+        ];
 
+    } else {
+        $GLOBALS['TCA']['tt_content']['types']['fsc_slider'] = [
+            'showitem' => '
+                --palette--;' . $frontendLanguageFilePrefix . 'palette.general;general,
+                image_format,
+                --palette--;' . $languageFilePrefix . 'tt_content.palette.mediaAdjustments;mediaAdjustments,
+                pi_flexform,
+                bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:bodytext_formlabel;bodytext,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+                --div--;' . $customLanguageFilePrefix . 'tca.tab.sliderElements,
+                 assets
+        ',
+            'columnsOverrides' => [
+                'media' => [
+                    'label' => $languageFilePrefix . 'tt_content.media_references',
+                    'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig('media', [
+                        'appearance' => [
+                            'createNewRelationLinkTitle' => $languageFilePrefix . 'tt_content.media_references.addFileReference'
+                        ],
+                        // custom configuration for displaying fields in the overlay/reference table
+                        // behaves the same as the image field.
+                        'foreign_types' => $GLOBALS['TCA']['tt_content']['columns']['image']['config']['foreign_types']
+                    ], $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext'])
+                ]
+            ]
+        ];
+    }
 });
+
+
+
+
