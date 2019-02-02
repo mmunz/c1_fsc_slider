1edd489b7d1b1337e36c1c8ee5c408a41bafedc5
Manuel Munz <manu@somakoma.de>
2019-02-02 12:16:42 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'CGLcompliance_note' => null,
     'constraints'        => [
         'depends'   => [
-            'typo3' => '7.6.0-8.99.99',
+            'typo3' => '7.6.0-9.5.99',
         ],
         'conflicts' => [],
         'suggests'  => []
